de9806ac3592f3d8ae6d6b6b8a8552b06e01299b
Emmanuel Obe <113615572+emobe6422@users.noreply.github.com>
2026-04-16 18:32:26 -0400

    v_2
    Made outputs into reg instead of wire. Added more immediates and used an always @(*) begin statement instead

--- decoder.v
+++ decoder.v
@@ -5,46 +5,36 @@
  module decoder(
 //start with R and I type. Later S and U
     input wire [31:0] instruction,
-    output wire [6:0] opcode,
+    output reg [6:0] opcode,
     //register destination
-    output wire [4:0] rd,
+    output reg [4:0] rd,
     //extra parameters for ALU operation (AND, OR, add, sub, etc.)
-    output wire [2:0] funct3,
-    output wire [6:0] funct7,
+    output reg [2:0] funct3,
+    output reg [6:0] funct7,
     //register sources #1 and #2
-    output wire [4:0] rs1,
-    output wire [4:0] rs2,
-    
- 
+    output reg [4:0] rs1,
+    output reg [4:0] rs2,
     //We don't need an immediate for R type since no immediates
-    output wire [31:0] Iimm //sign extended Immediate opcode
+    output reg [31:0] Iimm, //sign extended Immediate opcode
+    output reg [31:0] Simm,
+    output reg [31:0] Bimm
+    //output reg [31:0] Jimm
     );
-
-    //Get opcode
-    assign opcode = instruction[6:0];
-    //Get register destination
-    assign rd  = instruction[11:7];
-    //Get funct3 and funct7
-    assign funct3 = instruction[14:12];
-    assign funct7 = instruction[31:25]; 
-    //Get Source Resigters.
-    assign rs1 = instruction[19:15];
-    assign rs2 = instruction[24:20];
     
-
-    
-    
-    
-    
-    
-
-    /*
-    * RETURN TO IMMEDIATES
-    * SHOULD HAVE A MUX TO DECIDE WHICH IMM TO USE NEXT
-    * B type instructions next
-    * I is not shifted but B (branch) and J (jump) are
-    */
-    //assign immediates (signed sign extension)
-    //they also need that move left 1 bit thing
-    assign Iimm = {{21{instruction[31]}},instruction[30:20]};
+    always @(*) begin 
+        //get opcode
+        opcode = instruction[6:0];
+        //get register destination
+        rd  = instruction[11:7];
+        //Get funct3 and funct7
+        funct3 = instruction[14:12];
+        funct7 = instruction[31:25]; 
+        //get Source Resigters.
+        rs1 = instruction[19:15];
+        rs2 = instruction[24:20];
+        //assign immediates (signed sign extension)
+        Iimm = {{21{instruction[31]}},instruction[30:20]}; 
+        Simm = {{21{instruction[31]}},instruction[30:25],instruction[11:7]};
+        Bimm = {{20{instruction[31]}},instruction[7],instruction[30:25],instruction[11:8],1'b0};
+    end
 endmodule
