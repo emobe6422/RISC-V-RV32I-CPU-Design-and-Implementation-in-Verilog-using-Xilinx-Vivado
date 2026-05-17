9ae87a0aef14ae3c791f4b68e12ad6b7a30a88ef
Emmanuel Obe <113615572+emobe6422@users.noreply.github.com>
2026-05-17 13:29:39 -0400

    Update register_file.v

--- sources_1/new/register_file.v
+++ sources_1/new/register_file.v
@@ -9,12 +9,6 @@ module register_file(
     
     reg [31:0] registers [0:31];
     
-    integer i;
-    initial begin
-        for(i = 0; i < 32; i = i + 1)
-            registers[i] = 32'b0;
-    end
-
     always @(*) begin
         read_data_1 = (rs1 == 5'b00000) ? 32'b0 : registers[rs1];
         read_data_2 = (rs2 == 5'b00000) ? 32'b0 : registers[rs2];
